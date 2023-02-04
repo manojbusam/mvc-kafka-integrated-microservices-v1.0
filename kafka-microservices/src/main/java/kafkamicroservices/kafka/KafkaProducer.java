d0813b7a7ed07c3f75cb0448723bf31d368a6544
manojbusam <manojbusam@outlook.com>
2023-02-04 15:11:45 -0500

    adding producer

---
+++ kafka-microservices/src/main/java/kafkamicroservices/kafka/KafkaProducer.java
@@ -0,0 +1,22 @@
+package kafkamicroservices.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaProducer {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+}
