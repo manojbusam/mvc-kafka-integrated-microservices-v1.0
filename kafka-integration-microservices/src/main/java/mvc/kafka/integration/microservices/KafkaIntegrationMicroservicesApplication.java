0b2ebb70a030498a5625866f92067cd59cd7a718
manojbusam <manojbusam@outlook.com>
2023-02-04 10:52:36 -0500

    microservice test

---
+++ kafka-integration-microservices/src/main/java/mvc/kafka/integration/microservices/KafkaIntegrationMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package mvc.kafka.integration.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaIntegrationMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaIntegrationMicroservicesApplication.class, args);
+	}
+
+}
---
+++ kafka-integration-microservices/src/test/java/mvc/kafka/integration/microservices/KafkaIntegrationMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package mvc.kafka.integration.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaIntegrationMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
