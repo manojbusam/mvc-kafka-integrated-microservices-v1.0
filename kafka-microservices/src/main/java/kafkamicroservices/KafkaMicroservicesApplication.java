020dd40a4c77fca1d04e60e7420d2ede7a7da650
manojbusam <manojbusam@outlook.com>
2023-02-04 13:59:50 -0500

    microservice

---
+++ kafka-microservices/src/main/java/kafkamicroservices/KafkaMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package kafkamicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMicroservicesApplication.class, args);
+	}
+
+}
